af7c2abbe6e3dc44accc800261bd8be7cfbf1c5e
AMIR SOHEL <43946342+asb19@users.noreply.github.com>
2022-02-10 01:53:38 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
-exports.PORT = process.env.PORT || 3400;
+exports.PORT = process.env.PORT || 5000;
 exports.MONGODB_URI = process.env.MONGODB_URI || "mongodb+srv://1234:1234@cluster0.4k6kk.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
 exports.NODE_ENV = process.env.NODE_ENV || "development";
 
